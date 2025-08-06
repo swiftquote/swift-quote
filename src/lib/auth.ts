651726c70f7e5e92d1157100c8afa456240374a6
swiftquote <mismail17.308@gmail.com>
2025-08-07 00:52:40 +0100

    Fix GoogleProvider import - use default import

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,5 @@
 import { NextAuthOptions } from "next-auth"
-import { GoogleProvider } from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions: NextAuthOptions = {
   providers: [
